25a795ea4c448a8af9eedf5c7a543a665e601098
Brian Mak <eggrollbot@gmail.com>
2019-12-06 21:40:26 -0800

    Reorganized Blackjack class
    - Broke the startRound method down into components and reorganized the
    class' methods.

--- src/eggroll/apcsa/blackjack/Blackjack.java
+++ src/eggroll/apcsa/blackjack/Blackjack.java
@@ -11,8 +11,7 @@ public class Blackjack {
 	public Blackjack( ) { // Constructor
 	}
 	
-	public void askForAnotherRound( ) { // At the end of a round, ask if the human wants to play another round.
-		System.out.print( "Would you like to play another round? (y/n) " );
+	public boolean checkForPlayInput( ) { // Internal method to use the scanner to ask if the player wants to play.
 		String play = null;
 		
 		do { // Check for a valid input and try again if invalid.
@@ -23,20 +22,35 @@ public class Blackjack {
 			play = scanner.nextLine( );
 		} while ( !play.toLowerCase( ).equals( "y" ) && !play.toLowerCase( ).equals( "n" ) );
 		
-		if ( play.toLowerCase( ).equals( "n" ) ) {
+		if ( play.toLowerCase( ).equals( "y" ) ) { // Player said no.
+			return true;
+		} else {
 			System.out.println( "Okay bye!" );
-			return;
+			
+			return false;
 		}
-		
-		startRound( ); // Start a new round.
 	}
 	
-	public void startRound( ) {
-		deck = new Deck( );
-		ply = new Human( deck );
-		dealer = new Dealer( deck );
-		System.out.println( "The dealer drew a(n) " + dealer.getCard( 0 ) + ". Current hand value: " + dealer.getTotalHandValue( ) );
-		System.out.println( "You drew a(n) " + ply.getCard( 0 ) + " and a(n) " + ply.getCard( 1 ) + ". Current hand value: " + ply.getTotalHandValue( ) );
+	public void askForRoundAndStart( ) { // Ask for a round.
+		System.out.print( "Would you like to play a round of blackjack? (y/n) " );
+		boolean playInput = checkForPlayInput( );
+		System.out.println( );
+		
+		if ( playInput ) {
+			startRound( );
+		}
+	}
+	
+	public void askForAnotherRoundAndStart( ) { // At the end of a round, ask if the human wants to play another round.
+		System.out.print( "Would you like to play another round? (y/n) " );
+		boolean playInput = checkForPlayInput( );
+		
+		if ( playInput ) {
+			startRound( );
+		}
+	}
+	
+	public boolean loopHumanDecision( ) { // Will return whether the player can continue or busted.
 		String choice;
 		
 		do { // Loops while the human hits.
@@ -57,17 +71,17 @@ public class Blackjack {
 				
 				if ( ply.getTotalHandValue( ) > 21 ) {
 					System.out.println( "Player bust. You lost." ); // The human lost because they bust. :(
-					askForAnotherRound( ); // Ask the human for another round.
+					askForAnotherRoundAndStart( ); // Ask the human for another round.
 					
-					return;
+					return false;
 				}
 			}
 		} while ( choice.equals( "hit" ) );
 		
-		// Onto the dealer's turn.
-		
-		String card = dealer.revealSecondCard( ); // Draw the second card and reveal it.
-		System.out.println( "The dealer reveals a(n) " + card + ". Current hand value: " + dealer.getTotalHandValue( ) );
+		return true;
+	}
+	
+	public boolean loopDealerDecision( ) { // Will return whether the dealer can continue or busted.
 		String dealersChoice = null;
 		
 		do { // Loops while the dealer hits.
@@ -77,42 +91,76 @@ public class Blackjack {
 		
 		if ( dealer.getTotalHandValue( ) > 21 ) {
 			System.out.println( "Dealer bust. You win." ); // The dealer bust, so the human wins! :)
-			askForAnotherRound( ); // Ask the human for another round.
+			askForAnotherRoundAndStart( ); // Ask the human for another round.
 			
-			return;
+			return false;
 		}
 		
-		if ( ply.getTotalHandValue( ) > dealer.getTotalHandValue( ) ) { // Check who wins if no one bust.
-			System.out.println( "Player wins." );
-		} else if ( dealer.getTotalHandValue( ) > ply.getTotalHandValue( ) ) {
-			System.out.println( "Dealer wins." );
+		return true;
+	}
+	
+	public int checkWin( ) { // 1 = player, 2 = dealer, 3 = tie.
+		int plyHand = ply.getTotalHandValue( );
+		int dealerHand = dealer.getTotalHandValue( );
+		
+		if ( plyHand > dealerHand ) { // Check who wins if no one bust.
+			return 1;
+		} else if ( dealerHand > plyHand ) {
+			return 2;
 		} else {
+			return 3;
+		}
+	}
+	
+	public void startRound( ) {
+		// Initialize deck, player, and dealer.
+		
+		deck = new Deck( );
+		ply = new Human( deck );
+		dealer = new Dealer( deck );
+		
+		// Draw the initial cards.
+		
+		System.out.println( "The dealer drew a(n) " + dealer.getCard( 0 ) + ". Current hand value: " + dealer.getTotalHandValue( ) );
+		System.out.println( "You drew a(n) " + ply.getCard( 0 ) + " and a(n) " + ply.getCard( 1 ) + ". Current hand value: " + ply.getTotalHandValue( ) );
+		
+		// Have the human make their decision.
+		
+		if ( !loopHumanDecision( ) ) {
+			return; // Player busted.
+		}
+		
+		// Reveal the dealer's second card.
+		
+		String card = dealer.revealSecondCard( ); // Draw the dealer's second card and reveal it.
+		System.out.println( "The dealer reveals a(n) " + card + ". Current hand value: " + dealer.getTotalHandValue( ) );
+		
+		// Have the dealer make their decision.
+		
+		if ( !loopDealerDecision( ) ) {
+			return; // Dealer busted.
+		}
+		
+		// Check who won if no one busted.
+		
+		int checkWin = checkWin( );
+		
+		if ( checkWin == 1 ) { // Player win.
+			System.out.println( "Player wins." );
+		} else if ( checkWin == 2 ) { // Dealer win.
+			System.out.println( "Dealer wins." );
+		} else { // Player and Dealer tie.
 			System.out.println( "Player and dealer tie." );
 		}
 		
-		askForAnotherRound( ); // Ask the human for another round.
+		// Ask the human for another round and start.
+		
+		askForAnotherRoundAndStart( );
 	}
 	
 	public void playGame( ) { // Called by the Main class.
 		System.out.println( "Welcome to blackjack!" );
 		System.out.println( );
-		System.out.print( "Would you like to play a round of blackjack? (y/n) " );
-		String play = null;
-		
-		do { // Check for a valid input and try again if invalid.
-			if ( play != null ) { // Check if they're on the second time around.
-				System.out.print( "Invalid input. Enter y or n. " );
-			}
-			
-			play = scanner.nextLine( );
-		} while ( !play.toLowerCase( ).equals( "y" ) && !play.toLowerCase( ).equals( "n" ) );
-		
-		if ( play.toLowerCase( ).equals( "n" ) ) { // Aww, the player said no. :(
-			System.out.println( "Okay bye!" );
-			return;
-		}
-		
-		System.out.println( );
-		startRound( ); // Starts the round.
+		askForRoundAndStart( ); // Ask the human for a round and start.
 	}
 }
